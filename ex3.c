657483c9850e2dc748da4977b56a19c732d3c573
Miguel Rosa <73667613+miguelpedrorosa@users.noreply.github.com>
2023-05-25 12:50:53 +0100

    Update ex3.c

---
+++ ex3.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <pthread.h>
+
+typedef struct
+{
+    int value;
+    pthread_mutex_t mutex;
+    pthread_cond_t condition;
+} countdown_t;
+
+int countdown_init(countdown_t *cd, int initialValue)
+{
+    cd->value = initialValue;
+    pthread_mutex_init(&cd->mutex, NULL);
+    pthread_cond_init(&cd->condition, NULL);
+    return 0;
+}
+
+int countdown_destroy(countdown_t *cd)
+{
+    pthread_mutex_destroy(&cd->mutex);
+    pthread_cond_destroy(&cd->condition);
+    return 0;
+}
+
+int countdown_wait(countdown_t *cd)
+{
+    pthread_mutex_lock(&cd->mutex);
+    while (cd->value > 0)
+    {
+        pthread_cond_wait(&cd->condition, &cd->mutex);
+    }
+    pthread_mutex_unlock(&cd->mutex);
+    return 0;
+}
+
+int countdown_down(countdown_t *cd)
+{
+    pthread_mutex_lock(&cd->mutex);
+    cd->value--;
+    if (cd->value == 0)
+    {
+        pthread_cond_broadcast(&cd->condition);
+    }
+    pthread_mutex_unlock(&cd->mutex);
+    return 0;
+}
